079fd0b40b5d7cd8a53e936fc26d26df795d67cb
Schnarfengargle123 <schnarfengargle123@gmail.com>
2023-06-01 15:14:00 +0100

    Added `createProduct` logic to /products

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log("Request Body: ", req.body);
+    // res.sendStatus(201);
     res.send(req.body);
 })
 
@@ -20,6 +21,35 @@ app.get('/products', async (req, res) => {
     const products = await prisma.product.findMany();
     console.log("Products: ", products);
     res.send(products);
+})
+
+app.post('/products', async (req, res) => {
+    const {title, description, price, quantity, available, category} = req.body;
+
+    const products = await prisma.product.findMany();
+
+    const newProduct = await prisma.product.create({
+        // data: {
+        //     title: req.body.title,
+        //     description: req.body.description,
+        //     price: req.body.price,
+        //     quantity: req.body.quantity,
+        //     available: req.body.available
+        // }
+
+        data: {
+            title: title,
+            description: description,
+            price: price,
+            quantity: quantity,
+            available: available,
+            category: category
+        }
+    });
+
+    console.log("New Product: ", newProduct);
+    console.log("Products: ", products);
+    res.send({newProduct, products});
 })
 
 app.listen(8000, () => {
